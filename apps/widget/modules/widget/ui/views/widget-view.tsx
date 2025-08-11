105a7dd2b2acbe648849ba4a17618e0505be5414
harshbajani <harshbajani7@gmail.com>
2025-08-11 16:27:43 +0530

    11:  widget screen router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { WidgetFooter } from "../components/widget-footer";
-import { WidgetHeader } from "../components/widget-header";
+import { useAtomValue } from "jotai";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
+import { screenAtom } from "../../atoms/widget-atoms";
 
 interface Props {
   organizationId: string;
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>TODO: Error</p>,
+    loading: <p>TODO: Loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: voice</p>,
+    inbox: <p>TODO: inbox</p>,
+    selection: <p>TODO: selection</p>,
+    chat: <p>TODO: chat</p>,
+    contact: <p>TODO: contact</p>,
+  };
   return (
     <main className="flex min-h-screen min-w-screen h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      <WidgetFooter />
+      {screenComponents[screen]}
     </main>
   );
 };
