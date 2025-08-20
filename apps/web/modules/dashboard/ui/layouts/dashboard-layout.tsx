fc0cd8859a7dbf7401b2318bf632f63c0ba228d4
harshbajani <harshbajani7@gmail.com>
2025-08-20 15:09:50 +0530

    18: dashboard chat

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -4,6 +4,7 @@ import { SidebarProvider } from "@workspace/ui/components/sidebar";
 import { cookies } from "next/headers";
 import { ReactNode } from "react";
 import { DahsboardSidebar } from "../components/dashboard-sidebar";
+import { Provider } from "jotai";
 
 export const DashboardLayout = async ({
   children,
@@ -16,10 +17,12 @@ export const DashboardLayout = async ({
   return (
     <AuthGuard>
       <OrganizationGuard>
-        <SidebarProvider defaultOpen={defaultOpen}>
-          <DahsboardSidebar />
-          <main className="flex flex-1 flex-col">{children}</main>
-        </SidebarProvider>
+        <Provider>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <DahsboardSidebar />
+            <main className="flex flex-1 flex-col">{children}</main>
+          </SidebarProvider>
+        </Provider>
       </OrganizationGuard>
     </AuthGuard>
   );
